b61d5c23497aafce16663144d8bb6fc64230ae80
anilsah895 <anilsah895@gmail.com>
2026-04-26 02:27:55 +0545

    chore: remove unused utils.py

--- app/utils.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-
-def safe_embedding(text: str):
-    if not text:
-        return np.zeros(1536).tolist()
-
-    np.random.seed(abs(hash(text)) % (2**32))
-    return np.random.rand(1536).tolist()
